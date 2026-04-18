0a1e7cbd612b5269173cc076a03ef715b2c1f383
DeondraRatliff <Dratlif@iu.edu>
2026-04-17 20:38:10 -0500

    User.java

--- User.java
+++ User.java
@@ -4,10 +4,65 @@
  */
 package petcare;
 
-/**
- *
- * @author deondraratliff
- */
+/*
+ Project: Pet Care Reminder
+ Author: Deondra Ratliff
+ Date: 04/17/2026
+ Description: This class represents a user who owns pets and manages pet care reminders.
+*/
 public class User {
     
+// Attributes
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+
+    // Default constructor
+    public User() {
+    }
+
+
+    // Parameterized constructor
+    public User(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    // Gets the user's name
+    public String getName() {
+        return name;
+    }
+
+
+    // Sets the user's name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    // Gets the user's email
+    public String getEmail() {
+        return email;
+    }
+
+
+    // Sets the user's email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    // Gets the user's phone number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+    // Sets the user's phone number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
